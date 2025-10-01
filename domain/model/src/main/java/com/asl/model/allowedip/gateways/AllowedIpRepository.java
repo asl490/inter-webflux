cb195b37fd213a521a4156367a337f1ddd2ab76c
asl490 <asl490.np@gmail.com>
2025-10-01 17:10:47 -0500

    ip repository

--- domain/model/src/main/java/com/asl/model/allowedip/gateways/AllowedIpRepository.java
+++ domain/model/src/main/java/com/asl/model/allowedip/gateways/AllowedIpRepository.java
@@ -1,4 +1,20 @@
 package com.asl.model.allowedip.gateways;
 
+import com.asl.model.allowedip.AllowedIp;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface AllowedIpRepository {
+    Mono<AllowedIp> findById(String id);
+
+    Mono<AllowedIp> save(AllowedIp allowedIp);
+
+    Mono<Void> deleteById(String id);
+
+    Mono<AllowedIp> update(AllowedIp allowedIp);
+
+    Mono<AllowedIp> findByIp(String ipAddress);
+
+    Flux<AllowedIp> findAll();
 }
